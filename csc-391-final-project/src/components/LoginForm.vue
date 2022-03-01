3cf9c7d3b38adba5b5ad57732addf975d50dca42
fahnz <tyler.fahney@mail.wlc.edu>
2022-03-01 11:45:30 -0600

    started working on login page, need to do alot of styling yet, form is pretty much done

--- csc-391-final-project/src/components/LoginForm.vue
+++ csc-391-final-project/src/components/LoginForm.vue
@@ -1,6 +1,18 @@
 <template>
     <span>
+        <div id="login">
+            <form>
+                <h3>Sign in</h3>
+                <div class="form-group">
+                    <label>Email address</label>
+                    <input type ="email" placeholder="Email">
+                    <label>Password</label>
+                    <input type ="password" placeholder="Password">
+                    <v-btn>Login</v-btn>
+                </div>
+            </form>
 
+        </div>
     </span>
 </template>
 
@@ -11,5 +23,14 @@ export default {
 </script>
 
 <style scoped>
+
+input[type=email], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
 
 </style>
