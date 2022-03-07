c58d6380deff03d7b4e9b7f4f25c3ee819e317e4
Dylan Chirigotis <dylan.chirigotis@mail.wlc.edu>
2022-03-07 13:16:53 -0600

    Finished mock data and started functionality of table

--- csc-391-final-project/src/components/CommentsOrRecommendations.vue
+++ csc-391-final-project/src/components/CommentsOrRecommendations.vue
@@ -3,8 +3,9 @@
         <v-row>
             <v-col sm='12' md='4' v-for='commentOrRecommendation in commentOrRecommendationData' :key='commentOrRecommendation.id'>
                 <v-card-title>{{'Name: ' + commentOrRecommendation.name}}</v-card-title>
-                <v-card-text>{{'Stock/Crypto Currency:' + commentOrRecommendation.stockOrCrypto}}</v-card-text>
-                <v-card-text>{{'Comment:' + commentOrRecommendation.description}}</v-card-text>
+                <v-card-text>{{'Username: ' + commentOrRecommendation.username}}</v-card-text>
+                <v-card-text>{{'Stock/Crypto Currency: ' + commentOrRecommendation.stockOrCrypto}}</v-card-text>
+                <v-card-text>{{'Comment: ' + commentOrRecommendation.description}}</v-card-text>
             </v-col>
         </v-row>
     </v-container>
@@ -12,6 +13,12 @@
 
 <script>
 import commentOrRecommendationInfo from '@/data/commentRecommendationInfo.json';
+
+/*
+    Steps to Complete
+    1) Add Styling
+*/
+
 export default {
     name: 'CommentsOrRecommendations',
     data() {
--- csc-391-final-project/src/components/StockCryptoTable.vue
+++ csc-391-final-project/src/components/StockCryptoTable.vue
@@ -5,13 +5,26 @@
         :items='stockCryptoData'
         :items-per-page='10'
         class='elevation-1'
-    /><v-data-footer />
+        :footer-props="{
+            showFirstLastPage: true,
+            firstIcon: 'mdi-arrow-collapse-left',
+            lastIcon: 'mdi-arrow-collapse-right',
+            prevIcon: 'mdi-minus',
+            nextIcon: 'mdi-plus',
+        }"
+    >
+    </v-data-table>
     </span>
 </template>
 
 <script>
 import stockCryptoInfo from '@/data/stockCryptoInfo.json';
-// add name on Table
+/*
+    Steps to Complete
+    1) Add Pagination to Table
+    2) Add Sorting to Table
+    3) Style Table
+*/
 export default {
     name: 'StockCryptoTable',
     data () {
@@ -24,6 +37,7 @@ export default {
                     value: 'num',
                 },
                 { text: 'Symbol', value: 'symbol' },
+                { text: 'Name (EN)', value: 'name' },
                 { text: 'Current Price ($)', value: 'current_price' },
                 { text: 'Last Opening Price ($)', value: 'opening_price' },
                 { text: 'Last Closing Price ($)', value: 'closing_price' },
