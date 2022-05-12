8aaadee1fdd51fdd3c754de1247489fba61020a4
fahnz <tyler.fahney@mail.wlc.edu>
2022-05-12 12:20:45 -0500

    added photo

--- csc-391-final-project/src/views/About.vue
+++ csc-391-final-project/src/views/About.vue
@@ -2,6 +2,7 @@
     <div class='about'>
         <h1 class='text-center'>About Our Project</h1>
         <br>
+        <v-img class='about-logo-img' max-height='300' max-width='300' :src='require("@/assets/stockCryptoLogo.png")' />
         <br>
         <h3>Team Members</h3>
         <ul>
